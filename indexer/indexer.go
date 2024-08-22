ed408209db0bee857acc4c4054251e907b5aefb4
Carsten Zeumer <carsten.zeumer@autonubil.de>
2024-08-22 13:33:20 +0200

    implement query vulnerabilities from opensearch

---
+++ indexer/indexer.go
@@ -0,0 +1,80 @@
+package indexer
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	opensearch "github.com/opensearch-project/opensearch-go"
+)
+
+type Indexer struct {
+	client *opensearch.Client
+
+	IndexName string
+}
+
+func New(client *opensearch.Client) (*Indexer, error) {
+	return &Indexer{
+		client:    client,
+		IndexName: "wazuh-states-vulnerabilities-wazuh",
+	}, nil
+}
+
+type agentVulnerabilitySource struct {
+	Vulnerability AgentVulnerability `json:"_source,omitempty"`
+}
+
+type agentVulnerabilityHit struct {
+	Hits []agentVulnerabilitySource `json:"hits,omitempty"`
+}
+type osResult struct {
+	Hits agentVulnerabilityHit `json:"hits,omitempty"`
+}
+
+func (i *Indexer) GetAgentVulnerabilities(agentId string, ctx context.Context) ([]AgentVulnerability, error) {
+
+	query := fmt.Sprintf(`{
+	"query": {
+		"term": {
+			"agent.id": "%s"
+		}
+	}
+}`, agentId)
+
+	res, err := i.client.Search(
+		i.client.Search.WithContext(ctx),
+		i.client.Search.WithIndex(i.IndexName),
+		i.client.Search.WithBody(strings.NewReader(query)),
+		i.client.Search.WithFrom(0),
+		i.client.Search.WithSize(2500),
+		i.client.Search.WithRequestCache(true),
+		i.client.Search.WithSort("vulnerability.id:asc"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if res != nil && res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(res.Body)
+	raw := buf.String()
+
+	result := osResult{}
+	err = json.Unmarshal(([]byte)(raw), &result)
+	if err != nil {
+		return nil, err
+	}
+
+	vulns := []AgentVulnerability{}
+	for _, hit := range result.Hits.Hits {
+		vulns = append(vulns, hit.Vulnerability)
+	}
+	return vulns, nil
+
+}
--- ossec/agent_test.go
+++ ossec/agent_test.go
@@ -3,10 +3,12 @@ package ossec
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestAgentClient(t *testing.T) {
-	a, err := NewAgent("127.0.0.1", "666", "zbook-cze", "c588215eddc469001c41f4aa5cc306ea20fc6815e5cfe3cc666c5ccb7bc505e0", WithAgentAllowedIPs("any"), WithEncryptionMethod(EncryptionMethodBlowFish))
+	a, err := NewAgent("wazuh.opsanio-elk-mimosa.exanio.net", "400444", "exanio-degustator-test-t0-test-444", "UrR55N38Q5JtY73244532312222224216k9KcCTpfA2zem7U81hg26w3An47j6w9", WithAgentAllowedIPs("any"), WithEncryptionMethod(EncryptionMethodAES), WithTCP(true))
+	// a, err := NewAgent("10.1.42.2", "232333", "exanio-degustator-test-t0-test-033", "UrR55N38Q5JtY73244534YKcaTKfw6N46k9KcCTpfA2zem7U81hg26w3An47j6w9", WithAgentAllowedIPs("any"), WithEncryptionMethod(EncryptionMethodAES), WithTCP(true))
 	if err != nil {
 		t.Error(err)
 		return
@@ -43,4 +45,21 @@ func TestEncoding(t *testing.T) {
 		return
 	}
 	fmt.Printf("Decrypted: %s\n", decrypted)
+}
+
+func TestAw(t *testing.T) {
+
+	a, err := NewAgent("server.agent-cluster1.prod.eu001-prod.arcticwolf.net", "3538182", "e5df31e7-a005-49de-a4ca-4a76386cc49b_3fab09e7-53ad-4a9d-8005-e1e8ab4834d8", "radvEIB3KahGdwA0vw8mT9o2LSRRMiLSdPjyDgRHoF6phqkghF2S98hRU8Nud2QC", WithAgentIP("any"), WithTCP(true), WithEncryptionMethod(EncryptionMethodAES))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer a.Close()
+	err = a.Connect(true)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(10 * time.Minute)
 }
